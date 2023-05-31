1d7d16ccf2a2c859f380332692d8634d868a127c
adrianadyrda <adrianadyrda99@gmail.com>
2023-05-31 23:02:33 +0300

    Homework 8 committed

---
+++ Homework 8/Birthdays.py
@@ -0,0 +1,33 @@
+from datetime import datetime, timedelta
+def get_birthdays_per_week(users):
+    start_date = datetime.today() - timedelta(days=2)
+    start_date = start_date + timedelta(days=(7 - start_date.weekday()))
+    end_date = start_date + timedelta(days=7)
+    birthdays = {}
+    for user in users:
+        b_day = user['birthday']
+        b_day = datetime(year=start_date.year, month=b_day.month, day=b_day.day)
+        if start_date <= b_day <= end_date:
+            if b_day.weekday() == 5:
+                b_day += timedelta(days=2)
+            elif b_day.weekday() == 6:
+                b_day += timedelta(days=1)
+            if b_day not in birthdays.keys():
+                birthdays[b_day] = []
+            birthdays[b_day].append(user['name'])
+
+    for date in birthdays.keys():
+        day = date.strftime('%A')
+        names = birthdays[date]
+        names_result = ','.join(names)
+        print(f'{day}: {names_result}')
+
+
+
+
+
+
+print(get_birthdays_per_week([{'name':'Bill','birthday':datetime(1998,6,11)},{'name':'Kelli','birthday':datetime(1998,10,15)}, {'name':'Jessi','birthday':datetime(2020,6,11)}]))
+
+
+
